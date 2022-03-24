cf5efac411e555be34f5cea40304dfbe71e261e4
sakchaisrim <sakchaisrim@gmail.com>
2022-03-24 09:21:45 +0700

    commit3

--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -15,24 +15,23 @@ class DashboardScreen extends StatefulWidget {
 }
 
 class _DashboardScreenState extends State<DashboardScreen> {
+  // สร้าง Object SharedPreferences
+  SharedPreferences sharedPreferences;
 
- // สร้าง Object SharedPreferences 
- SharedPreferences sharedPreferences;
-
- // สร้างตัวแปร ไว้เก็บชื่อและอีเมล์ผู้ใช้
- String nameAccount, emailAccount;
+  // สร้างตัวแปร ไว้เก็บชื่อและอีเมล์ผู้ใช้
+  String nameAccount, emailAccount;
 
   // สร้างตัวแปร List เก็บรายการหน้าที่ต้องการเปลี่ยนใน Bottom Navigationbar
- int _currentIndex = 0;
- String _title;
+  int _currentIndex = 0;
+  String _title;
 
- final List<Widget> _children = [
-   HomeScreen(),
-   StockScreen(),
-   NotificationScreen(),
-   SettingScreen(),
-   AccountScreen()
- ];
+  final List<Widget> _children = [
+    HomeScreen(),
+    StockScreen(),
+    NotificationScreen(),
+    SettingScreen(),
+    AccountScreen()
+  ];
 
   @override
   void initState() {
@@ -46,7 +45,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
   checkLoginStatus() async {
     sharedPreferences = await SharedPreferences.getInstance();
 
-    if(sharedPreferences.getString("storeEmail") == null){
+    if (sharedPreferences.getString("storeEmail") == null) {
       // ส่งกลับไปหน้า Login
       Navigator.pushNamed(context, '/login');
     }
@@ -55,135 +54,150 @@ class _DashboardScreenState extends State<DashboardScreen> {
       nameAccount = sharedPreferences.getString('storeName');
       emailAccount = sharedPreferences.getString('storeEmail');
     });
-    
   }
 
- // สร้าง method สำหรับการเปลี่ยนหน้า
- void onTabTapped(int index){
-   setState(() {
-     _currentIndex = index;
+  // สร้าง method สำหรับการเปลี่ยนหน้า
 
-    switch (index) {
-      case 0: _title = 'หน้าหลัก';
-        break;
-      case 1: _title = 'สต๊อกสินค้า';
-        break;
-      case 2: _title = 'แจ้งเตือน';
-        break;
-      case 3: _title = 'ตั้งค่า'; 
-        break;
-      case 4: _title = 'ฉัน'; 
-        break;
-    }
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
 
-   });
- }
+      switch (index) {
+        case 0:
+          _title = 'หน้าหลัก';
+          break;
+        case 1:
+          _title = 'สต๊อกสินค้า';
+          break;
+        case 2:
+          _title = 'แจ้งเตือน';
+          break;
+        case 3:
+          _title = 'ตั้งค่า';
+          break;
+        case 4:
+          _title = 'ฉัน';
+          break;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       appBar: AppBar(
-         title: Text(_title, style: GoogleFonts.kanit(),),
-         actions: <Widget>[
-           FlatButton(
-             onPressed: (){
-               // เคลียร์ค่าใน sharedPreferences
-               sharedPreferences.clear();
-               Navigator.pushNamed(context, '/login');
-             }, 
-             child: Text("ออกจากระบบ", style: TextStyle(color: Colors.white),)
-            )
-         ],
-       ),
-       
-       // ส่วนของ DrawerMenu
-      drawer: SafeArea(
-        child: Drawer(
-          child: ListView(
-            children: <Widget>[
-              UserAccountsDrawerHeader(
-                currentAccountPicture: GestureDetector(
-                  onTap: () { },
-                  child: CircleAvatar(
-                    backgroundImage: NetworkImage('https://image.freepik.com/free-vector/profile-icon-male-avatar-hipster-man-wear-headphones_48369-8728.jpg'),
-                  ),
-                ),
-                otherAccountsPictures: <Widget>[
-                GestureDetector(
-                    onTap: () { },
-                    child: CircleAvatar(
-                      backgroundImage: NetworkImage('https://images.megapixl.com/4707/47075236.jpg'),
-                    ),
-                )
-                ],
-                accountName: Text('$nameAccount'), 
-                accountEmail: Text('$emailAccount'),
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: NetworkImage('https://www.vickyalvearshecter.com/wp-content/uploads/2015/02/2012-06-08_0000066-as-Smart-Object-1-600x400.jpg'),
-                    fit: BoxFit.fill
-                  )
-                ),
-              ),
-              ListTile(
-                leading: Icon(Icons.info),
-                title: Text('เกี่ยวกับเรา'),
-                onTap: (){
-                  Navigator.pop(context);
-                  Navigator.pushNamed(context, '/about');
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.book),
-                title: Text('ข้อมูลการใช้งาน'),
-                onTap: (){
-                  Navigator.pop(context);
-                  Navigator.pushNamed(context, '/term');
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.mail),
-                title: Text('ติดต่อทีมงาน'),
-                onTap: (){ 
-                  Navigator.pop(context);
-                  Navigator.pushNamed(context, '/contact');
-                },
-              ),
-              ListTile(
-                leading: Icon(Icons.exit_to_app),
-                title: Text('ออกจากระบบ'),
-                onTap: (){ 
-                  // เคลียร์ค่าใน sharedPreferences
-                  sharedPreferences.clear();
-                  Navigator.pushNamed(context, '/login');
-                },
-              ),
-              Divider(color: Colors.green[200],),
-              ListTile(
-                leading: Icon(Icons.cancel),
-                title: Text('ปิดเมนู'),
-                onTap: () => Navigator.of(context).pop(),
-              ),
-            ],
-          ),
-        )
+      appBar: AppBar(
+        title: Text(
+          _title,
+          style: GoogleFonts.kanit(),
+        ),
+        actions: <Widget>[
+          FlatButton(
+              onPressed: () {
+                // เคลียร์ค่าใน sharedPreferences
+                sharedPreferences.clear();
+                Navigator.pushNamed(context, '/login');
+              },
+              child: Text(
+                "ออกจากระบบ",
+                style: TextStyle(color: Colors.white),
+              ))
+        ],
       ),
 
-       body: _children[_currentIndex],
+      // ส่วนของ DrawerMenu
+      drawer: SafeArea(
+          child: Drawer(
+        child: ListView(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              currentAccountPicture: GestureDetector(
+                onTap: () {},
+                child: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      'https://image.freepik.com/free-vector/profile-icon-male-avatar-hipster-man-wear-headphones_48369-8728.jpg'),
+                ),
+              ),
+              otherAccountsPictures: <Widget>[
+                GestureDetector(
+                  onTap: () {},
+                  child: CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        'https://images.megapixl.com/4707/47075236.jpg'),
+                  ),
+                )
+              ],
+              accountName: Text('$nameAccount'),
+              accountEmail: Text('$emailAccount'),
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: NetworkImage(
+                          'https://www.vickyalvearshecter.com/wp-content/uploads/2015/02/2012-06-08_0000066-as-Smart-Object-1-600x400.jpg'),
+                      fit: BoxFit.fill)),
+            ),
+            ListTile(
+              leading: Icon(Icons.info),
+              title: Text('เกี่ยวกับเรา'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.pushNamed(context, '/about');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.book),
+              title: Text('ข้อมูลการใช้งาน'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.pushNamed(context, '/term');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.mail),
+              title: Text('ติดต่อทีมงาน'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.pushNamed(context, '/contact');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.exit_to_app),
+              title: Text('ออกจากระบบ'),
+              onTap: () {
+                // เคลียร์ค่าใน sharedPreferences
+                sharedPreferences.clear();
+                Navigator.pushNamed(context, '/login');
+              },
+            ),
+            Divider(
+              color: Colors.green[200],
+            ),
+            ListTile(
+              leading: Icon(Icons.cancel),
+              title: Text('ปิดเมนู'),
+              onTap: () => Navigator.of(context).pop(),
+            ),
+          ],
+        ),
+      )),
+
+      body: _children[_currentIndex],
 
       // ส่วนของ Bottom Navigation Bar
       bottomNavigationBar: BottomNavigationBar(
-       onTap: onTabTapped,
-       currentIndex: _currentIndex,
-       type: BottomNavigationBarType.fixed,
-       items: <BottomNavigationBarItem>[
-         BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('หน้าหลัก')),
-         BottomNavigationBarItem(icon: Icon(Icons.storage), title: Text('สต็อก')),
-         BottomNavigationBarItem(icon: Icon(Icons.notifications), title: Text('แจ้งเตือน')),
-         BottomNavigationBarItem(icon: Icon(Icons.settings), title: Text('ตั้งค่า')),
-         BottomNavigationBarItem(icon: Icon(Icons.person), title: Text('ฉัน')),
-       ]
-      ),
+          onTap: onTabTapped,
+          currentIndex: _currentIndex,
+          type: BottomNavigationBarType.fixed,
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home), title: Text('หน้าหลัก')),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.storage), title: Text('สต็อก')),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.notifications), title: Text('แจ้งเตือน')),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.settings), title: Text('ตั้งค่า')),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person), title: Text('ฉัน')),
+          ]),
     );
   }
 }
